ed04bb6c74661a76b70cd776c3e341984d1c4712
LordDetson <babanin.dima@gmail.com>
2019-01-12 10:41:51 +0300

    Create new simple data type variables and it initialize

--- Homework1/src/ru/geekbrains/Homework.java
+++ Homework1/src/ru/geekbrains/Homework.java
@@ -1,6 +1,14 @@
 package ru.geekbrains;
 
 public class Homework {
+    private static byte aByte = 27;
+    private static short aShort = 120;
+    private static int aInt = 180;
+    private static long aLong = 45L;
+    private static float aFloat = 13.58F;
+    private static double aDouble = 94.8426;
+    private static char aChar = 'd';
+    private static boolean aBoolean = false;
 
     public static void main(String[] args) {
 
